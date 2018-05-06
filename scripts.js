c91992f2f936f0c0f80c37a8ae34f35aa3c54296
vasudevapitta <tizzicboy@gmail.com>
2018-05-06 18:15:58 -0400

    Responsive Portfolio Website
    I coded this mobile responsive portfolio website. I Improvised the prototype's design & functionality to make the site more dynamic rather than static. I used HTML5, CSS3, JavaScript with ES6 syntax and jQuery.

---
+++ scripts.js
@@ -0,0 +1,38 @@
+$(()=>{
+$(".progressbar-fullonezero").toggleClass("progressbar-fullone");
+$(".progressbar-fulltwozero").toggleClass("progressbar-fulltwo");
+$(".progressbar-fullthreezero").toggleClass("progressbar-fullthree");
+$(".progressbar-fullfourzero").toggleClass("progressbar-fullfour");
+$(".progressbar-fullfivezero").toggleClass("progressbar-fullfive");
+$(".progressbar-fullsixzero").toggleClass("progressbar-fullsix");
+$(".progressbar-fullsevenzero").toggleClass("progressbar-fullseven");
+
+$(".box-one").click(function(){
+window.open ("https://www.behance.net/vasudevapitta","_blank");
+});
+
+$(".box-two").click(function(){
+window.open ("https://codepen.io/vasudevapitta/full/MVZaoG/","_blank");
+});
+
+$(".box-three").click(function(){
+window.open ("https://codepen.io/vasudevapitta/full/wmYKgp/","_blank");
+});
+
+$(".facebook").click(function(){
+window.open ("https://www.facebook.com/pvrpavankumar","_blank");
+});
+
+$(".fa-linkedin").click(function(){
+window.open ("https://www.linkedin.com/in/vasudevapitta","_blank");
+});
+
+$(".fa-google-plus-square").click(function(){
+window.open ("https://plus.google.com/u/0/+VasudevaPitta","_blank");
+});
+
+$(".fa-envelope").click(function(){
+window.open ("mailto:tizzicboy@gmail.com?subject=subject&body=body","_blank");
+});
+
+});
