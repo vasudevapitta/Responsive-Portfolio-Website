6f03c8020457a46e194362e6e36078a2461c86dd
vasudevapitta <tizzicboy@gmail.com>
2018-05-07 11:34:03 -0400

    organized folders, comments & tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,37 @@ $(".progressbar-fullfivezero").toggleClass("progressbar-fullfive");
 $(".progressbar-fullsixzero").toggleClass("progressbar-fullsix");
 $(".progressbar-fullsevenzero").toggleClass("progressbar-fullseven");
 
+//link to open Behance Portfolio
 $(".box-one").click(function(){
 window.open ("https://www.behance.net/vasudevapitta","_blank");
 });
 
+//link to open Shopping List App
 $(".box-two").click(function(){
 window.open ("https://codepen.io/vasudevapitta/full/MVZaoG/","_blank");
 });
 
+//link to open Pixel Art Maker
 $(".box-three").click(function(){
 window.open ("https://codepen.io/vasudevapitta/full/wmYKgp/","_blank");
 });
 
+//link to open my Facebook Page
 $(".facebook").click(function(){
 window.open ("https://www.facebook.com/pvrpavankumar","_blank");
 });
 
+//link to open Linkedin URL
 $(".fa-linkedin").click(function(){
 window.open ("https://www.linkedin.com/in/vasudevapitta","_blank");
 });
 
+//link to open Google Plus URL
 $(".fa-google-plus-square").click(function(){
 window.open ("https://plus.google.com/u/0/+VasudevaPitta","_blank");
 });
 
+//link to open email - Currently not assigned
 $(".fa-envelope").click(function(){
 window.open ("mailto:tizzicboy@gmail.com?subject=subject&body=body","_blank");
 });
